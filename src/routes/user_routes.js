5b44fdcf9ab4fdfde0755ea89880678ac218a7c9
Abhishek <abhishekmore1806@gmail.com>
2024-01-12 17:22:55 +0530

    Add new routes for user_controllers

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -1,8 +1,21 @@
 // user_routes.js
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user_controller.js";
+import { 
+        loginUser, 
+        logoutUser, 
+        registerUser, 
+        refreshAccessToken, 
+        changeCurrentPassword, 
+        getCurrentUser, 
+        updateAccountDetails, 
+        updateUserAvatar, 
+        updateUserCoverImage, 
+        getUserChannelProfile, 
+        getWatchHistory 
+    } from "../controllers/user_controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJwt } from "../middlewares/auth.middleware.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const userRouter = Router();
 
@@ -24,7 +37,18 @@ userRouter.route("/login").post(loginUser)
 
 //secured routes
 userRouter.route("/logout").post(verifyJwt, logoutUser)
-
 userRouter.route("/refresh-token").post(refreshAccessToken)
+userRouter.route("change-password").post(verifyJwt,changeCurrentPassword)
+userRouter.route("/current-user").get(verifyJwt,getCurrentUser)
+userRouter.route("/update-account-details").patch(verifyJwt,updateAccountDetails)
+
+userRouter.route("/update-avatar").patch(verifyJwt,upload.single("avatar"),updateUserAvatar)
+
+userRouter.route("/update-coverImage").patch(verifyJwt,upload.single("coverImage"),updateUserCoverImage)
+
+
+userRouter.route("/c/:username").get(verifyJwt,getUserChannelProfile)
+
+userRouter.route("/history").get(verifyJwt,getWatchHistory)
 
 export default userRouter;
